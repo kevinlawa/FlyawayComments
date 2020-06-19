b389d0888324642e9b2ac018a0b94271ebdadf97
Kevin Pham <kpham@lawa.org>
2020-06-19 13:27:40 -0700

    Added README.md

--- FlyawayComments.Functions/Startup.cs
+++ FlyawayComments.Functions/Startup.cs
@@ -5,9 +5,7 @@ using FlyawayComment.Functions.Models;
 using FlyawayComments.Data.Repositories;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 [assembly: FunctionsStartup(typeof(FlyawayComment.Functions.Startup))]
 
