4a2c09cd49d1b2668bc29782067b5da9e6855d3b
Kevin Pham <kpham@lawa.org>
2020-06-19 03:31:34 -0700

    Added date parameter to filter comments by date added

--- FlyawayComment.Functions/FlyawayComments.cs
+++ FlyawayComment.Functions/FlyawayComments.cs
@@ -27,17 +27,24 @@ namespace FlyawayComment.Functions
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string date = req.Query["date"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            date = date ?? data?.date;
+
+            var dateAdded = default(DateTime);
+
+            var isDate = DateTime.TryParse(date, out dateAdded);
+            if (!isDate)
+                dateAdded = DateTime.Now;
+
 
             //string responseMessage = string.IsNullOrEmpty(name)
             //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
             //    : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-            var comments = repo.GetFlyawayComments(DateTime.Now.AddDays(-30).Date).ToList();
+            var comments = repo.GetFlyawayComments(dateAdded).ToList();
 
             return new OkObjectResult(comments);
         }
